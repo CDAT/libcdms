a48a743754bdc527debd6881d7ba6b02b38e9da4
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-17 00:32:08 -0700

    use label unstable

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -29,5 +29,5 @@ rm -rf uvcdat
 export BRANCH=${CIRCLE_BRANCH}
 python ./prep_for_build.py  -b ${BRANCH}
 
-conda build ${PKG_NAME} -c conda-forge 
+conda build ${PKG_NAME} -c cdat/label/unstable -c conda-forge 
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l $LABEL $CONDA_BLD_PATH/$OS/${PKG_NAME}-$VERSION.`date +%Y*`0.tar.bz2 --force
